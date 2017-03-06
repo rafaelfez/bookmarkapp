fc2d01be5131d0883f05dbf1232fc881dbb73d5e
rafaelfez <rafafelipesilva@gmail.com>
2017-03-06 11:19:50 -0300

    Validations

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ function saveBookmark(e){
   var siteName= document.getElementById('siteName').value;
   var siteUrl= document.getElementById('siteUrl').value;
 
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
 
   var bookmark = {
     name: siteName,
@@ -37,6 +40,9 @@ function saveBookmark(e){
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
   }
+
+  //clear form
+  document.getElementById('myForm').reset();
 
   //re-fetch bookmarks
   fetchBookmarks();
@@ -82,4 +88,21 @@ function deleteBookmark(url){
 
   //re-fetch bookmarks
   fetchBookmarks();
+}
+
+function validateForm(siteName, siteUrl){
+
+  if(!siteName || !siteUrl){
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid URL');
+    return false;
+  }
+  return true;
 }
